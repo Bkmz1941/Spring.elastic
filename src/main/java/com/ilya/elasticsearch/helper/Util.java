9b6c33f99fabce01e0fe2f87d431e6d39f46380a
Ilya Titov <bkmz1941f@gmail.com>
2024-06-29 18:56:05 +0200

    feat: work with resources was changed

--- src/main/java/com/ilya/elasticsearch/helper/Util.java
+++ src/main/java/com/ilya/elasticsearch/helper/Util.java
@@ -4,18 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 public class Util {
     private static final Logger LOG = LoggerFactory.getLogger(Util.class);
 
     public static String loadAsString(final String path) {
         try {
-            final File resource = new ClassPathResource(path).getFile();
-            return new String(Files.readAllBytes(resource.toPath()));
+            String content;
+            ClassPathResource resource  = new ClassPathResource(path);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+            content = reader.lines().collect(Collectors.joining("\n"));
+            return content;
         } catch (final Exception e) {
             LOG.error(e.getMessage(), e);
             return null;
